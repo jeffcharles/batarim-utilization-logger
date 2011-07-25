8d7d93c2216284571a23a10c45c5d4f468b4944a
Jeffrey Charles <jeffreycharles@gmail.com>
2011-07-24 21:12:22 -0400

    Describe get_ram_usage function

--- src/ram_usage.h
+++ src/ram_usage.h
@@ -5,6 +5,7 @@ typedef struct {
     int percentage_usage;
 } ram_usage_struct;
 
+// Populates the given ram_usage_struct with ram usage information
 void get_ram_usage(ram_usage_struct*);
 
 #endif
