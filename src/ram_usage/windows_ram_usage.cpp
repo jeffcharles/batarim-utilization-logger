23e26fa0c5cf4c1874a8d7cd479675b1b5a37453
Jeffrey Charles <jeffreycharles@gmail.com>
2011-08-07 19:31:51 -0400

    Add in better error handling in RAM usage code

--- src/ram_usage/windows_ram_usage.cpp
+++ src/ram_usage/windows_ram_usage.cpp
@@ -1,11 +1,25 @@
+#include <iostream>
 #include <windows.h>
 
 #include "ram_usage.hpp"
+
+using std::cerr;
+using std::clog;
+using std::endl;
+using std::hex;
 
 int get_ram_usage()
 {
     MEMORYSTATUSEX statex;
     statex.dwLength = sizeof(statex);
     int method_success = GlobalMemoryStatusEx(&statex);
-    return method_success ? (int)statex.dwMemoryLoad : -1;
+    if(method_success) {
+        return (int)statex.dwMemoryLoad;
+    } else {
+        cerr << "Could not determine memory status. Error code = 0x" << hex 
+            << method_success << endl;
+        clog << "Could not determine memory status. Error code = 0x" << hex 
+            << method_success << endl;
+        return -1;
+    }
 }
