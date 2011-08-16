41f7176552134ec6e5ac8ec332d52183e7b94ab1
Jeffrey Charles <jeffreycharles@gmail.com>
2011-08-16 18:36:52 -0400

    Get project to build on Linux using GCC

--- src/cpu_usage/unix_cpu_usage.cpp
+++ src/cpu_usage/unix_cpu_usage.cpp
@@ -1,7 +1,15 @@
+#include <string>
+#include <vector>
+
 #include "cpu_usage.hpp"
 
+using std::pair;
+using std::vector;
+using std::wstring;
+
 // TODO: make this calculate CPU usage
-void get_cpu_usage(int* usage_percentage)
+void get_cpu_usage(vector<pair<wstring, int> >& usage_percentages)
 {
-    *usage_percentage = 101;
+    pair<wstring, int> usage_percentage(L"1", 101);
+    usage_percentages.push_back(usage_percentage);
 }
