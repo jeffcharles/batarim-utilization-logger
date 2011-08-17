44bf34ee379bf6810771a91197e09b1211c012e5
Jeffrey Charles <jeffreycharles@gmail.com>
2011-08-17 13:41:10 -0400

    Add "sort-of" working implementation for retrieving Linux RAM usage

--- src/ram_usage/linux_ram_usage.cpp
+++ src/ram_usage/linux_ram_usage.cpp
@@ -1,7 +1,27 @@
+#include <iostream>
+#include <sys/sysinfo.h>
+
 #include "ram_usage.hpp"
 
-// TODO: actually make this work properly
+using std::endl;
+using std::cerr;
+using std::clog;
+
 int get_ram_usage()
 {
-    return -1;
+    // TODO: get more accurate reading by subtracting cached memory
+    // try reading and parsing entries from /proc/meminfo
+    struct sysinfo meminfo;
+    int result = sysinfo(&meminfo);
+    if(result == 0) {
+        unsigned long used_memory = 
+            meminfo.totalram - meminfo.freeram - meminfo.bufferram;
+        int ram_usage_percentage = 
+            (double)used_memory / meminfo.totalram * 100;
+        return ram_usage_percentage;
+    } else {
+        cerr << "Error retrieving ram usage" << endl;
+        clog << "Error retrieving ram usage" << endl;
+        return -1;
+    }
 }
