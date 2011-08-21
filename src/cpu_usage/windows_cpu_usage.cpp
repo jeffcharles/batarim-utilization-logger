5ab8e119d72d7107e8c6cf48a286c0c73f27cbd7
Jeffrey Charles <jeffreycharles@gmail.com>
2011-08-21 15:54:05 -0400

    Move "private" module methods into anonymous namespaces

--- src/cpu_usage/windows_cpu_usage.cpp
+++ src/cpu_usage/windows_cpu_usage.cpp
@@ -16,45 +16,49 @@ using std::vector;
 using std::wstring;
 using std::wstringstream;
 
-wstring get_human_readable_name(wstring wmi_name)
-{
-    int num;
-    wstringstream wss;
-    wss << wmi_name;
-    
-    bool is_number = wss >> num != 0;
-    if(is_number) {
-        wstringstream wss2;
-        wss2 << ++num;
-        
-        wstring ret;
-        wss2 >> ret;
-        return ret;
-    }
-    
-    if(wmi_name[0] == '_') {
-        return wmi_name.substr(1);
-    } else {
-        return wmi_name;
-    }
-}
+namespace {
 
-void fill_usage_percentages(
-    PDH_FMT_COUNTERVALUE_ITEM* processor_utilizations,
-    DWORD num_of_processor_entries,
-    vector<pair<wstring, int> >& usage_percentages
-)
-{
-    for(DWORD i = 0; i < num_of_processor_entries; ++i) {
-        string sname = processor_utilizations[i].szName;
-        wstring name;
-        name.assign(sname.begin(), sname.end());
-        pair<wstring, int> processor_utilization(
-            get_human_readable_name(name),
-            (int)processor_utilizations[i].FmtValue.largeValue
-        );
-        usage_percentages.push_back(processor_utilization);
+    wstring get_human_readable_name(wstring wmi_name)
+    {
+        int num;
+        wstringstream wss;
+        wss << wmi_name;
+    
+        bool is_number = wss >> num != 0;
+        if(is_number) {
+            wstringstream wss2;
+            wss2 << ++num;
+        
+            wstring ret;
+            wss2 >> ret;
+            return ret;
+        }
+    
+        if(wmi_name[0] == '_') {
+            return wmi_name.substr(1);
+        } else {
+            return wmi_name;
+        }
     }
+
+    void fill_usage_percentages(
+        PDH_FMT_COUNTERVALUE_ITEM* processor_utilizations,
+        DWORD num_of_processor_entries,
+        vector<pair<wstring, int> >& usage_percentages
+    )
+    {
+        for(DWORD i = 0; i < num_of_processor_entries; ++i) {
+            string sname = processor_utilizations[i].szName;
+            wstring name;
+            name.assign(sname.begin(), sname.end());
+            pair<wstring, int> processor_utilization(
+                get_human_readable_name(name),
+                (int)processor_utilizations[i].FmtValue.largeValue
+            );
+            usage_percentages.push_back(processor_utilization);
+        }
+    }
+
 }
 
 void get_cpu_usage(vector<pair<wstring, int> >& usage_percentages)
--- src/windows_pdh_wrapper/pdh_wrapper.cpp
+++ src/windows_pdh_wrapper/pdh_wrapper.cpp
@@ -9,90 +9,95 @@
 
 using std::string;
 
-void populate_query(const string& context, PDH_HQUERY& query)
-{
-    PDH_STATUS pdh_status = PdhOpenQuery(
-        NULL, // name of log file (NULL = use real-time data)
-        NULL, // user-defined value to associate with query
-        &query // handle to query
-    );
-    if(pdh_status != ERROR_SUCCESS) {
-        throw PdhException("PdhOpenQuery", context, pdh_status);
-    }
-}
+namespace {
 
-void add_counter_to_query(
-    const string& context,
-    PDH_HQUERY& query,
-    PDH_HCOUNTER& counter,
-    char* counter_path
-) {
-    PDH_STATUS pdh_status = PdhAddEnglishCounter(
-        query, // handle to query
-        counter_path, // counter path
-        NULL, // user-defined value (part of counter information)
-        &counter // handle to counter
-    );
-    if(pdh_status != ERROR_SUCCESS) {
-        throw PdhException("PdhAddEnglishCounter", context, pdh_status);
-    }
-}
-
-void collect_query_data(const string& context, PDH_HQUERY& query)
-{
-    PDH_STATUS pdh_status = PdhCollectQueryData(query);
-    if(pdh_status != ERROR_SUCCESS) {
-        throw PdhException("PdhCollectQueryData", context, pdh_status);
+    void populate_query(const string& context, PDH_HQUERY& query)
+    {
+        PDH_STATUS pdh_status = PdhOpenQuery(
+            NULL, // name of log file (NULL = use real-time data)
+            NULL, // user-defined value to associate with query
+            &query // handle to query
+        );
+        if(pdh_status != ERROR_SUCCESS) {
+            throw PdhException("PdhOpenQuery", context, pdh_status);
+        }
     }
 
-    Sleep(1000); // need to wait at least one second before querying again
-
-    // query again to get second data point 
-    // (see http://msdn.microsoft.com/en-us/library/aa371897(v=vs.85).aspx)
-    pdh_status = PdhCollectQueryData(query);
-    if(pdh_status != ERROR_SUCCESS) {
-        throw PdhException("PdhCollectQueryData", context, pdh_status);
+    void add_counter_to_query(
+        const string& context,
+        PDH_HQUERY& query,
+        PDH_HCOUNTER& counter,
+        char* counter_path
+    ) {
+        PDH_STATUS pdh_status = PdhAddEnglishCounter(
+            query, // handle to query
+            counter_path, // counter path
+            NULL, // user-defined value (part of counter information)
+            &counter // handle to counter
+        );
+        if(pdh_status != ERROR_SUCCESS) {
+            throw PdhException("PdhAddEnglishCounter", context, pdh_status);
+        }
     }
-}
 
-DWORD get_required_buffer_size(const string& context, PDH_HCOUNTER& counter)
-{
-    DWORD number_of_bytes_required = 0;
-    DWORD buffer_count;
-    PDH_FMT_COUNTERVALUE_ITEM empty_items;
-    PDH_STATUS pdh_status = PdhGetFormattedCounterArray(
-        counter, // handle to counter
-        PDH_FMT_LARGE, // format as 64-bit integers
-        &number_of_bytes_required, // required size of buffer
-        &buffer_count, // number of items returned
-        &empty_items // result buffer
-    );
-    if(pdh_status != PDH_MORE_DATA) {
-        throw PdhException("PdhGetFormattedCounterArray", context, 
-            pdh_status);
-    }
-    return number_of_bytes_required;
-}
+    void collect_query_data(const string& context, PDH_HQUERY& query)
+    {
+        PDH_STATUS pdh_status = PdhCollectQueryData(query);
+        if(pdh_status != ERROR_SUCCESS) {
+            throw PdhException("PdhCollectQueryData", context, pdh_status);
+        }
 
-void populate_result_set(
-    const string& context,
-    PDH_HCOUNTER& counter,
-    DWORD buffer_size, 
-    PDH_FMT_COUNTERVALUE_ITEM*& result_set,
-    DWORD& num_items_returned
-)
-{
-    PDH_STATUS pdh_status = PdhGetFormattedCounterArray(
-        counter, // handle to counter
-        PDH_FMT_LARGE, // format as 64-bit integers
-        &buffer_size, // required size of buffer
-        &num_items_returned, // number of items returned
-        result_set // result buffer
-    );
-    if(pdh_status != ERROR_SUCCESS) {
-        throw PdhException("PdhGetFormattedCounterArray", context, 
-            pdh_status);
+        Sleep(1000); // need to wait at least one second before querying again
+
+        // query again to get second data point 
+        // (see http://msdn.microsoft.com/en-us/library/aa371897(v=vs.85).aspx)
+        pdh_status = PdhCollectQueryData(query);
+        if(pdh_status != ERROR_SUCCESS) {
+            throw PdhException("PdhCollectQueryData", context, pdh_status);
+        }
     }
+
+    DWORD get_required_buffer_size(
+        const string& context,
+        PDH_HCOUNTER& counter
+    ) {
+        DWORD number_of_bytes_required = 0;
+        DWORD buffer_count;
+        PDH_FMT_COUNTERVALUE_ITEM empty_items;
+        PDH_STATUS pdh_status = PdhGetFormattedCounterArray(
+            counter, // handle to counter
+            PDH_FMT_LARGE, // format as 64-bit integers
+            &number_of_bytes_required, // required size of buffer
+            &buffer_count, // number of items returned
+            &empty_items // result buffer
+        );
+        if(pdh_status != PDH_MORE_DATA) {
+            throw PdhException("PdhGetFormattedCounterArray", context, 
+                pdh_status);
+        }
+        return number_of_bytes_required;
+    }
+
+    void populate_result_set(
+        const string& context,
+        PDH_HCOUNTER& counter,
+        DWORD buffer_size, 
+        PDH_FMT_COUNTERVALUE_ITEM*& result_set,
+        DWORD& num_items_returned
+    ) {
+        PDH_STATUS pdh_status = PdhGetFormattedCounterArray(
+            counter, // handle to counter
+            PDH_FMT_LARGE, // format as 64-bit integers
+            &buffer_size, // required size of buffer
+            &num_items_returned, // number of items returned
+            result_set // result buffer
+        );
+        if(pdh_status != ERROR_SUCCESS) {
+            throw PdhException("PdhGetFormattedCounterArray", context, 
+                pdh_status);
+        }
+    }
+
 }
 
 void cleanup(PDH_HQUERY& query, PDH_FMT_COUNTERVALUE_ITEM*& result_set)
