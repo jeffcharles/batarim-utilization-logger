7948929c5378224e05b8c9605fb5f6ff96441bf8
Jeffrey Charles <jeffreycharles@gmail.com>
2011-08-25 19:56:09 -0400

    Add active window CPU usage calculation implementation

--- src/active_window/LinuxActiveWindow.hpp
+++ src/active_window/LinuxActiveWindow.hpp
@@ -1,6 +1,7 @@
 #ifndef GUARD_LinuxActiveWindow_h
 #define GUARD_LinuxActiveWindow_h
 
+#include <ios>
 #include <memory>
 #include <string>
 
@@ -31,6 +32,11 @@ class LinuxActiveWindow : public IActiveWindow
     private:
         std::shared_ptr<Display> display_;
         Window window_;
+
+        std::shared_ptr<std::istream> get_system_stat_line();
+        unsigned long get_system_time(std::istream& stat_line);
+        std::shared_ptr<std::istream> get_stat_stream();
+        unsigned long get_process_cpu_time(std::istream& stat_stream);
 };
 
 #endif
