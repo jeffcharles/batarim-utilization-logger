bf2c0d2019dd92e22afce649360ebccc205cae4b
Jeffrey Charles <jeffreycharles@gmail.com>
2011-09-02 15:42:18 -0400

    Simplify Windows get_ram_usage implementation

--- src/ram_usage/windows_ram_usage.cpp
+++ src/ram_usage/windows_ram_usage.cpp
@@ -1,25 +1,10 @@
-#include <iostream>
 #include <windows.h>
 
 #include "ram_usage.hpp"
 
-using std::cerr;
-using std::clog;
-using std::endl;
-using std::hex;
-
 int get_ram_usage()
 {
-    MEMORYSTATUSEX statex;
-    statex.dwLength = sizeof(statex);
-    int method_success = GlobalMemoryStatusEx(&statex);
-    if(method_success) {
-        return (int)statex.dwMemoryLoad;
-    } else {
-        cerr << "Could not determine memory status. Error code = 0x" << hex 
-            << method_success << endl;
-        clog << "Could not determine memory status. Error code = 0x" << hex 
-            << method_success << endl;
-        return -1;
-    }
+    MEMORYSTATUS memory_status;
+    GlobalMemoryStatus(&memory_status);
+    return memory_status.dwMemoryLoad;
 }
