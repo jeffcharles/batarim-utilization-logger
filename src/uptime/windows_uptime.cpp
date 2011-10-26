df4cb9a8617c9e40bcb48b0c05cd431500d0f304
Jeffrey Charles <jeffreycharles@gmail.com>
2011-10-25 20:04:07 -0400

    Disable loss of precision warning on uptime in MSVC

--- src/uptime/windows_uptime.cpp
+++ src/uptime/windows_uptime.cpp
@@ -4,5 +4,11 @@
 
 unsigned int get_uptime()
 {
+#pragma warning(disable: 4244)
+    // 4,294,967,295 seconds
+    // / 60 sec/min / 60 min/hr / 24 hr/day / ~365 day/yr = ~136 years
+    // No one who is running this software should have an uptime higher than
+    // 136 years, so disregard the possible loss of data warning
     return GetTickCount64() / 1000;
+#pragma warning(default: 4244)
 }
